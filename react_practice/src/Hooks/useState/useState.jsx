c416138d2228b63541e750bcd995f9d17dbab64f
maruf5051 <marufsikder5051@gmail.com>
2025-12-03 21:56:05 +0600

    complete useState. Here i create three button named increment decrement and reset. and all button are working nicely. and also add condition the increment max 100 and dicrement min 0. and add a input filed step that help the counter

--- react_practice/src/Hooks/useState/useState.jsx
+++ react_practice/src/Hooks/useState/useState.jsx
@@ -1,14 +1,60 @@
+import "./hooks.css";
 import { useState } from "react";
 
 export const Counter = () => {
   const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  // handle increment function
+  const handleIncrement = () => {
+    setCount(count + step);
+  };
+
+  // handle decrement function
+  const handleDecrement = () => {
+    setCount(count - step);
+  };
+
+  // handle reset function
+  const handleReset = () => {
+    setCount(0);
+  };
+
   return (
-    <>
+    <div className="container">
       <div>
         <h1>Hooks useStae</h1>
       </div>
-      <h3>{count}</h3>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
-    </>
+      <h3>
+        Count: <span>{count}</span>
+      </h3>
+      <div>
+        <label>Step:</label>
+        <input
+          type="number"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+      </div>
+      <div>
+        <button
+          className="control-button"
+          onClick={handleIncrement}
+          disabled={count >= 100}
+        >
+          Increment
+        </button>
+        <button
+          className="control-button"
+          onClick={handleDecrement}
+          disabled={count <= 0}
+        >
+          Decrement
+        </button>
+        <button className="control-button" onClick={handleReset}>
+          Resent
+        </button>
+      </div>
+    </div>
   );
 };
