04d505a7b43635ff5631dc6074c653768b729f26
maruf5051 <marufsikder5051@gmail.com>
2025-12-04 02:34:33 +0600

    I start useEffect hooks

--- react_practice/src/App.jsx
+++ react_practice/src/App.jsx
@@ -1,14 +1,16 @@
 // import { Counter } from "./Hooks/useState/useState";
 // import { RegistrationForm } from "./Hooks/useState/registration";
+// import { LoginForm } from "./Hooks/useState/login";
 
-import { LoginForm } from "./Hooks/useState/login";
+import { ReactUseEffect } from "./Hooks/useEffect";
 
 export const App = () => {
   return (
     <section>
       {/* <Counter /> */}
       {/* <RegistrationForm /> */}
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <ReactUseEffect />
     </section>
   );
 };
